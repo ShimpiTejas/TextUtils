af1781f349a71f33f26e85c9a98cf7fd45e3c0f4
Tejas <shimpit45@gmail.com>
2023-06-20 23:01:30 +0530

    First react app succesfully made

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap";
+import { Hero } from './Hero';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hero.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import InputGroup from "react-bootstrap/InputGroup";
+
+
+
+export const Hero = (props) => {
+
+    let textData;
+  const [text, setText] = useState("");
+
+  const handleUpClick = (e) => {
+    textData = text.toUpperCase();
+    setText(textData);
+  };
+
+  return (
+    <div>
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">TextUtils</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">About</Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+      <div className="container  my-5">
+        <InputGroup>
+          <Form.Control
+            as="textarea"
+            rows={10}
+            placeholder="Enter the text to analyze"
+            aria-label="With textarea"
+            value={text}
+            onChange={(e) => {
+                setText(e.target.value)
+            }}
+          />
+        </InputGroup>
+
+        <Button variant="primary mx-2 my-2" onClick={handleUpClick}>
+          Change to Upper Case
+        </Button>
+        <Button variant="primary mx-2 my-2" onClick={()=>{textData = text.toLowerCase(); setText(textData) }}>Change to Lower case</Button>
+        <Button variant="primary mx-2 my-2">Summarize</Button>
+      </div>
+    </div>
+  );
+};
